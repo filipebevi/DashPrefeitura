b1d5fd9d6edf8888c6ac50b4846c1402b6dc6741
Filipe <filipebevi@hotmail.com>
2020-05-26 18:53:16 -0300

    mudança de nome

---
+++ src/main/java/com/mycompany/dashprefeitura/expense/Expense.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.dashprefeitura.expense;
+
+/**
+ *
+ * @author Flipe
+ */
+public class Expense {
+    
+    private int mes;
+    private String category;
+    private String source;
+    private double valor;
+
+    /**
+     * @return the mes
+     */
+    
+    
+    
+    public Expense() {
+    }
+
+    /**
+     * @return the mes
+     */
+    public int getMes() {
+        return mes;
+    }
+
+    /**
+     * @param mes the mes to set
+     */
+    public void setMes(int mes) {
+        this.mes = mes;
+    }
+
+    /**
+     * @return the category
+     */
+    public String getCategory() {
+        return category;
+    }
+
+    /**
+     * @param category the category to set
+     */
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    /**
+     * @return the source
+     */
+    public String getSource() {
+        return source;
+    }
+
+    /**
+     * @param source the source to set
+     */
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    /**
+     * @return the valor
+     */
+    public double getValor() {
+        return valor;
+    }
+
+    /**
+     * @param valor the valor to set
+     */
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+   
+    
+}
